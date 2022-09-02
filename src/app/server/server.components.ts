5f80b6a9e10fc2d589a7a31e9a3f4b37ec8998eb
harbaz3301 <harbazbrar3223@gmail.com>
2022-09-02 14:04:29 +0530

    property and event binding, string interpolation

--- src/app/server/server.components.ts
+++ src/app/server/server.components.ts
@@ -1,9 +1,15 @@
+import { style } from "@angular/animations";
 import { Component } from "@angular/core";
 
 @Component({
    selector: 'app-server',
-   templateUrl: './server.component.html'
+   templateUrl: './server.component.html',
 })
 export class ServerComponent{
- 
+   serverstatus: string = "online"
+   serverId: number = 10
+
+   getserverstatus(){
+      return this.serverstatus;
+   }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: '[app-servers]',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowserver = false;
+  servercreation = 'no server is created';
+  servername = '';
+  constructor() { 
+    setTimeout(() => {
+      this.allowserver = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer(){
+    this.servercreation = 'server created';
+  }
+
+  oninput(event: Event)
+  {
+    console.log(event)
+    this.servername = (<HTMLInputElement>event.target).value;
+  }
 }
